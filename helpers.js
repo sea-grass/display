8d1bf540259c3ec56e8c2a902affa6be961624c3
sea-grass <dev.seagrass@gmail.com>
2017-08-15 15:04:09 -0400

    Remove Cel dependency

--- helpers.js
+++ helpers.js
@@ -1,19 +1,22 @@
-  function assert() {
-    if (arguments.length < 2) throw "Assert didn't receive enough args.";
+function assert() {
+  if (arguments.length < 2) throw "Assert didn't receive enough args.";
 
-    var message = arguments[0];
-    var failed = false;
-    for (var i = 1; i < arguments.length; i++) {
-      if (!arguments[i]) {
-        failed = true;
-        break;
-      }
-    }
-    if (failed) {
-      throw "Assert failed: " + message;
+  var message = arguments[0];
+  var failed = false;
+  for (var i = 1; i < arguments.length; i++) {
+    if (!arguments[i]) {
+      failed = true;
+      break;
     }
   }
-
-  function to1d(x, y, n) {
-    return y * n + x;
+  if (failed) {
+    throw "Assert failed: " + message;
   }
+}
+
+function to1d(x, y, n) {
+  return y * n + x;
+}
+
+var q = document.querySelector.bind(document),
+  create = document.createElement.bind(document);
