6766570a56a0224d7c37ab7632ff07e25a7fb5d4
sea-grass <dev.seagrass@gmail.com>
2020-10-09 17:51:54 -0400

    Move app to subfolder and add dev server

---
+++ scripts/dev.js
@@ -0,0 +1,24 @@
+const https = require('https');
+const express=require('express');
+const path=require('path');
+const fs = require('fs');
+require('dotenv').config();
+
+startDevServer(
+    process.env.DEV_PRIVATE_KEY,
+    process.env.DEV_PRIVATE_CERT,
+    process.env.DEV_PORT,
+    path.join(__dirname, '../public')
+)
+
+function startDevServer(key, cert, port, staticDir) {
+    console.log("startDevServer", arguments);
+    const app = express();
+    app.use(express.static(staticDir));
+    const options = {
+        key: fs.readFileSync(key),
+        cert: fs.readFileSync(cert)
+    }
+    https.createServer(options, app).listen(port);
+    console.log("Dev server listening at " + "https://localhost:"+port);
+}
